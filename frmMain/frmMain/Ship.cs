6658bed9d9435fa49e81159ee921b5b844b311a9
Thraelic <harryjrobertson@gmail.com>
2016-09-23 16:54:53 +0100

    Further tweaks to Ship and PlayerShip now adds a picture to picture box

--- frmMain/frmMain/Ship.cs
+++ frmMain/frmMain/Ship.cs
@@ -9,7 +9,7 @@ namespace frmMain
      public class Ship
     {
        protected byte _Health;
-       protected string _ShipImageAddress;
+       protected string _ShipImageAddress = "";
        protected string _BulletImageAddress = "";
 
        public static void FireBullet(bool EnemyFire, Form1 frm) //If EnemyFire = True then fire is from an enemy and can damage player
