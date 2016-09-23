5186a87eee6556a22100e58c742c2f25d585c60b
Thraelic <harryjrobertson@gmail.com>
2016-09-23 13:32:35 +0100

    Added Classes for Ships + Inheritance.

---
+++ frmMain/frmMain/Ship.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frmMain
+{
+     public class Ship
+    {
+       private byte _Health;
+       private string _ShipImageAddress;
+       private string _BulletImageAddress = "";
+
+       public static void FireBullet(bool EnemyFire) //If EnemyFire = True then fire is from an enemy and can damage player
+       {
+
+       }
+    }
+
+}
