579465669c28b7163337ce618358d5649f674cff
Thraelic <harryjrobertson@gmail.com>
2016-09-24 19:21:16 +0100

    Fixed _ShipImageAddress to work with any directory

---
+++ frmMain/frmMain/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+public struct Bullet
+{
+    private int ID;
+    enum BulletType {Friendly, Enemy}
+}
+
+namespace frmMain
+{
+    public partial class Form1 : Form
+    {
+        PlayerShip Player1 = new PlayerShip();
+        PlayerShip Player2 = new PlayerShip();
+        private bool TwoPlayer;
+
+        public Form1()
+        {
+            TwoPlayer = false;
+            InitializeComponent();
+            List<Bullet> Bullets = new List<Bullet>();
+            if (TwoPlayer)
+            {
+                Player1.LoadPlayerShip(this);
+                Player2.LoadPlayerShip(this);
+            }
+            else
+            {
+               
+                Player1.LoadPlayerShip(this);
+                Player2 = null;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        /*I should be able to encapsulate this!
+        * If i do i can have another player with their own controls e.g. If instantiated with 1 as parameter then responds to WASD if 2 then Up,Down,Left,Right otherwise allow both
+         * Done!
+        */
+
+        private void frmMain_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (TwoPlayer)
+            {
+                Player1.KeyDown(e, 1);
+                Player2.KeyDown(e, 2);
+            }
+            else
+            {
+                Player1.KeyDown(e, 3);
+            }
+
+            if (e.KeyCode == Keys.Escape)
+            {
+                Application.Exit();
+            }
+        } 
+
+        private void frmMain_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (TwoPlayer)
+            {
+                Player1.KeyUp(e, 1);
+                Player2.KeyUp(e, 2);
+            }
+            else
+            {
+                Player1.KeyUp(e, 3);
+            }
+        } //Same again
+
+        private void GameTick_Tick(object sender, EventArgs e)
+        {
+            /* Used for:
+             * Collision checks
+             * Enemy Movement / Firing
+             */
+            
+            //There has to be a better way to do this!
+            if (TwoPlayer)
+            {
+                Player1.ActionCheck(this);
+                Player2.ActionCheck(this);
+            }
+            else
+            {
+                Player1.ActionCheck(this);
+            }
+        }
+    }
+}
---
+++ frmMain/frmMain/PlayerShip.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace frmMain
+{
+    class PlayerShip : Ship
+    {
+        private bool _moveUp, _moveDown, _moveLeft, _moveRight,_spaceBar,_shift = false;
+        private int _velocity = 10;
+        PictureBox PlayerPictureBox = new PictureBox(); //Can be put in parent class but for simplicity leaving it here
+
+        public void LoadPlayerShip(Form1 frm)
+        {
+            _Health = 5;
+            _BulletImageAddress = @"\PlayerAssets";
+            _ShipImageAddress = AppDomain.CurrentDomain.BaseDirectory + @"\\PlayerAssets\Player.png";
+
+            PlayerPictureBox.Location = new Point(20 , 20);
+            PlayerPictureBox.Name = "PlayerPictureBox";
+            PlayerPictureBox.Image = new Bitmap(_ShipImageAddress);
+            PlayerPictureBox.Size = new Size(PlayerPictureBox.Image.Size.Width, PlayerPictureBox.Image.Size.Height);
+            frm.Controls.Add(PlayerPictureBox);
+        }
+
+        public bool MoveUp
+        {
+            get
+            {
+                return _moveUp;
+            }
+            set
+            {
+                _moveUp = value;
+            }
+        }
+
+        public bool MoveDown
+        {
+            get
+            {
+                return _moveDown;
+            }
+            set
+            {
+                _moveDown = value;
+            }
+        }
+
+        public bool MoveRight
+        {
+            get
+            {
+                return _moveRight;
+            }
+            set
+            {
+                _moveRight = value;
+            }
+        }
+
+        public bool MoveLeft
+        {
+            get
+            {
+                return _moveLeft;
+            }
+            set
+            {
+                _moveLeft = value;
+            }
+        }
+
+        public bool SpaceBar
+        {
+            get
+            {
+                return SpaceBar;
+            }
+            set
+            {
+                _spaceBar = value;
+            }
+        }   //Fire
+
+        public bool Shift
+        {
+            get
+            {
+                return _shift;
+            }
+            set
+            {
+                _shift = value;
+            }
+        }      //Boost
+
+        public void KeyDown(KeyEventArgs e,int controlsAllowed) //1 = WASD, 2 = UpDownLeftRight, 3 = Both
+        {
+            if (controlsAllowed == 1)
+            {
+
+            }
+            else if (controlsAllowed == 2)
+            {
+
+            }
+            else if (controlsAllowed == 3)
+            {
+                if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+                {
+                   _moveUp = true;
+                }
+
+                if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+                {
+                    _moveLeft = true;
+                }
+
+                if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+                {
+                    _moveRight = true;
+                }
+
+                if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+                {
+                    _moveDown = true;
+                }
+                if (e.KeyCode == Keys.ShiftKey)
+                {
+                    _shift = true;
+                }
+                if (e.KeyCode == Keys.Space)
+                {
+                    _spaceBar = true;
+                }
+            }
+        }
+
+        public void KeyUp(KeyEventArgs e, int controlsAllowed)
+        {
+            if (controlsAllowed == 1)
+            {
+
+            }
+            else if (controlsAllowed == 2)
+            {
+
+            }
+            else if (controlsAllowed == 3)
+            {
+                if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+                {
+                    _moveUp = false;
+                }
+
+                if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+                {
+                    _moveLeft = false;
+                }
+
+                if (e.KeyCode == Keys.Right || e.KeyCode == Keys.D)
+                {
+                    _moveRight = false;
+                }
+
+                if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+                {
+                    _moveDown = false;
+                }
+                if (e.KeyCode == Keys.ShiftKey)
+                {
+                    _shift = false;
+                }
+                if (e.KeyCode == Keys.Space)
+                {
+                    _spaceBar = false;
+                }
+            }
+        }
+
+        public void ActionCheck(Form1 frm)
+        {
+            if (_moveUp)
+            {
+                PlayerPictureBox.Location = new Point(PlayerPictureBox.Location.X, PlayerPictureBox.Location.Y - _velocity); //Negative is Up
+            }
+            if (_moveDown)
+            {
+                PlayerPictureBox.Location = new Point(PlayerPictureBox.Location.X, PlayerPictureBox.Location.Y + _velocity);
+            }
+            if (_moveLeft)
+            {
+                PlayerPictureBox.Location = new Point(PlayerPictureBox.Location.X - _velocity, PlayerPictureBox.Location.Y); //Negative is left
+            }
+            if (_moveRight)
+            {
+                PlayerPictureBox.Location = new Point(PlayerPictureBox.Location.X + _velocity, PlayerPictureBox.Location.Y);
+            }
+            if (_spaceBar)
+            {
+                Ship.FireBullet(false, frm);
+                _spaceBar = false;
+            }
+            if (_shift)
+            {
+                _velocity = 20;
+            }
+            else
+            {
+                _velocity = 5;
+            }
+        }
+    }
+}
